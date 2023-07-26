31a674a8945c212eb3c2271ecc0ed435910871a8
Kamlesh Chandnani <kamlesh.chandnani@gmail.com>
2023-07-26 17:33:24 +0530

    Initial commit

---
+++ src/Vote.jsx
@@ -0,0 +1,26 @@
+import { useState } from "react";
+
+export const Vote = () => {
+  const [voteCount, setVoteCount] = useState(0);
+
+  function updateVoteCount(type) {
+    if (type === "increment") {
+      setVoteCount((currentCount) => currentCount + 1);
+    } else if (type === "decrement") {
+      setVoteCount((currentCount) => currentCount - 1);
+    }
+  }
+  return (
+    <>
+      <h3>Vote your opinion</h3>
+      <button onClick={() => updateVoteCount("increment")}>upvote</button>
+      <p id="counter-value">{voteCount}</p>
+      <button onClick={() => updateVoteCount("decrement")}>
+        downvote{" "}
+        <span role="img" aria-label="thumbs-down">
+          👎
+        </span>
+      </button>
+    </>
+  );
+};
